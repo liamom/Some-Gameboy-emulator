dae814f96a5bf8ac9b921e97a513e342434728d5
liamom <lolivermallory123@pointloma.edu>
2015-05-26 23:48:41 -0700

    Removed the extra main function in the gameboy project

--- SomeGameboyEmulator/main.cpp
+++ SomeGameboyEmulator/main.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 int main()
 {
-	string cartrage_path = "../roms/alleyway (jua).gb";
-	cartridge test(cartrage_path);
+	string cartridge_path = "../roms/alleyway (jua).gb";
+	cartridge test(cartridge_path);
     test.dump_header_data();
 
 	cpu processorTest(test);
